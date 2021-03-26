6738c3675916425bee919a2376081e990c6f4983
Jay24004 <79038529+Jay24004@users.noreply.github.com>
2021-03-26 13:19:43 +0530

    updated the member_join only trigger on main server join not test server

--- TGK.py
+++ TGK.py
@@ -229,7 +229,7 @@ async def test(ctx, member:discord.Member):
 
 
 
-client.run(f'ODE2Njk5MTY3ODI0MjgxNjIx.YD-wXw.ZwQEyvDMWayQJdFeA9AXXgOxcyw')
+client.run(f'ODE2Njk5MTY3ODI0MjgxNjIx.YD-wXw.CE18KDWlS2fvjyBDNw_fRvonw_s')
 #ODE2Njk5MTY3ODI0MjgxNjIx.YD-wXw.ZwQEyvDMWayQJdFeA9AXXgOxcyw	
 
 #<a:celeyay:821818380406882315>
