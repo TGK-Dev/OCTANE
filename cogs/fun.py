cb5b602eba3c56f132aa472551d08b8ea5de4301
Jay24004 <79038529+Jay24004@users.noreply.github.com>
2021-04-02 06:36:20 -0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -227,7 +227,7 @@ class fun(commands.Cog):
 			await ctx.reply(embed=em)
 		else:
 			em = discord.Embed(color=usercolor, description=f"**{ctx.author.name}** is murdering **{member.name}**")
-			em.set_image(url=kill_list)
+			em.set_image(url=kill)
 			await ctx.reply(embed=em)
 
 	@commands.command(name="8Ball", description="Ask you question to bot", usage="[question]", aliases=['8b'])
