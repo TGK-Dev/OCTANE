c6422a5aaff1dcd6fbde7830f5ae73bada99b99a
Jay24004 <79038529+Jay24004@users.noreply.github.com>
2021-04-03 11:05:08 -0700

    Update warn.py

--- cogs/warn.py
+++ cogs/warn.py
@@ -47,13 +47,6 @@ async def GetMessage(
 class Warns(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-	
-	
-	
-    
-    @commands.Cog.listener()
-	async def on_ready(self):
-		print(f"{self.__class__.__name__} Cog has been loaded\n-----")
 		
     @commands.command(name="Warn", description="Gives an Warnings to user", usage="[member] [warn]")
     @commands.has_permissions(manage_messages=True)
