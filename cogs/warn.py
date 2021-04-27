bb6ff78a0145b1d42e1b42c872066b30e4b8c210
Jay Patel <pateljay24004@gmail.com>
2021-04-27 12:36:05 +0530

    Update warn.py

--- cogs/warn.py
+++ cogs/warn.py
@@ -6,69 +6,11 @@ import random
 from bson.objectid import ObjectId
 from discord.ext import commands
 from discord.ext.buttons import Paginator
+from utils.util import Pag
 from dateutil.relativedelta import relativedelta
 
 time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
 time_dict = {"h": 3600, "s": 1, "m": 60, "d": 86400}
-
-
-
-
-class Pag(Paginator):
-    async def teardown(self):
-        try:
-            await self.page.clear_reactions()
-        except discord.HTTPException:
-            pass
-
-async def GetMessage(
-    bot, ctx, contentOne="Default Message", contentTwo="\uFEFF", timeout=100
-):
-    """
-    This function sends an embed containing the params and then waits for a message to return
-    Params:
-     - bot (commands.Bot object) :
-     - ctx (context object) : Used for sending msgs n stuff
-     - Optional Params:
-        - contentOne (string) : Embed title
-        - contentTwo (string) : Embed description
-        - timeout (int) : Timeout for wait_for
-    Returns:
-     - msg.content (string) : If a message is detected, the content will be returned
-    or
-     - False (bool) : If a timeout occurs
-    """
-    embed = discord.Embed(title=f"{contentOne}", description=f"{contentTwo}",)
-    sent = await ctx.send(embed=embed)
-    try:
-        msg = await bot.wait_for(
-            "message",
-            timeout=timeout,
-            check=lambda message: message.author == ctx.author
-            and message.channel == ctx.channel,
-        )
-        if msg:
-            return msg.content
-    except asyncio.TimeoutError:
-        return False
-
-class TimeConverter(commands.Converter):
-    async def convert(self, ctx, argument):
-        args = argument.lower()
-        matches = re.findall(time_regex, args)
-        time = 0
-        for key, value in matches:
-            try:
-                time += time_dict[value] * float(key)
-            except KeyError:
-                raise commands.BadArgument(
-                    f"{value} is an invalid time key! h|m|s|d are valid arguments"
-                )
-            except ValueError:
-                raise commands.BadArgument(f"{key} is not a number!")
-        return round(time)
-
-
 
 class Warns(commands.Cog):
     def __init__(self, bot):
