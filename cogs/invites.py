38cd36b6cd93eb8e98bfcfc0383af30ab2685e7a
Jay Patel <pateljay24004@gmail.com>
2021-05-05 18:39:55 +0530

    added info for Cog/commands

--- cogs/invites.py
+++ cogs/invites.py
@@ -2,6 +2,7 @@ import discord
 import DiscordUtils
 import datetime
 from discord.ext import commands
+from utils.util import Pag
 from discord.ext.buttons import Paginator
 
 # Requires: pip install DiscordUtils
@@ -9,48 +10,9 @@ from discord.ext.buttons import Paginator
 def fomat_time(time):
     return time.strftime('%d-%B-%Y %I:%m %p')
 
+description = "Invites Systems"
 
-class Pag(Paginator):
-    async def teardown(self):
-        try:
-            await self.page.clear_reactions()
-        except discord.HTTPException:
-            pass
-
-            async def GetMessage(
-                bot, ctx, contentOne="Default Message", contentTwo="\uFEFF", timeout=100
-            ):
-                """
-                This function sends an embed containing the params and then waits for a message to return
-                Params:
-                 - bot (commands.Bot object) :
-                 - ctx (context object) : Used for sending msgs n stuff
-                 - Optional Params:
-                    - contentOne (string) : Embed title
-                    - contentTwo (string) : Embed description
-                    - timeout (int) : Timeout for wait_for
-                Returns:
-                 - msg.content (string) : If a message is detected, the content will be returned
-                or
-                 - False (bool) : If a timeout occurs
-                """
-                embed = discord.Embed(title=f"{contentOne}", description=f"{contentTwo}",)
-                sent = await ctx.send(embed=embed)
-                try:
-                    msg = await bot.wait_for(
-                        "message",
-                        timeout=timeout,
-                        check=lambda message: message.author == ctx.author
-                        and message.channel == ctx.channel,
-                    )
-                    if msg:
-                        return msg.content
-                except asyncio.TimeoutError:
-                    return False
-
-
-
-class Invites(commands.Cog):
+class Invites(commands.Cog, description=description):
     def __init__(self, bot):
         self.bot = bot
         self.tracker = DiscordUtils.InviteTracker(bot)
@@ -114,6 +76,7 @@ class Invites(commands.Cog):
             await ctx.send(embed=embed)
             
     @commands.command(name="inviter", description="find who Invited given user", usage="[member]")
+    @commands.has_any_role(785842380565774368,799037944735727636, 785845265118265376, 787259553225637889)
     async def inviter(self, ctx, member: discord.Member):
 
         invites_filter = {"userInvited": member.id}
@@ -129,7 +92,8 @@ class Invites(commands.Cog):
             embed = discord.Embed(description=f"The {member.mention} Is invited by <@{invier}>", timestamp=datetime.datetime.now())
 
             await ctx.send(embed=embed)
-    @commands.command(name='ilb', description="invite lb")
+            
+    @commands.command(name='ilb', description="invites leaderboard")
     async def ilb(self, ctx):
         invites = await self.bot.invites.get_all()
         
