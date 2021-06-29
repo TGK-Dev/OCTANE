167842d06bcbbd4054b95b34cafdff2337ec1fef
Jay Patel <pateljay24004@gmail.com>
2021-06-29 23:37:36 +0530

    fix broken inviter cmd

--- cogs/invites.py
+++ cogs/invites.py
@@ -94,21 +94,17 @@ class Invites(commands.Cog, description=description):
             
     @commands.command(name="inviter", description="Find out who invited who")
     @commands.check_any(perm_check(), is_me())
-    async def inviter(self, ctx, member):
+    async def inviter(self, ctx, member:discord.Member):
 
         invites_filter = {"userInvited": member.id}
 
-        invites = await self.bot.invites.find_many_by_custom(invites_filter)
+        data = await self.bot.invites.find_by_custom(invites_filter)
 
-        if not bool(invites):
+        if not bool(data):
             return await ctx.send("I failed to find vaild Entry in DataBase")
 
-        for invite in invites:
-            invier = (invite['_id'])
-            mcolor = member.color
-            embed = discord.Embed(description=f"The {member.mention} Is invited by <@{invier}>", timestamp=datetime.datetime.now())
-
-            await ctx.send(embed=embed)
+        embed = discord.Embed(color=0x2f3136, description=f"{member.mention} Was invited by <@{data['_id']}>")
+        await ctx.send(embed=embed)
 
     @commands.command(name='ilb', description="invites leaderboard")
     async def ilb(self, ctx):
