28f597a6c677afebd3cb87d6db015331b98ee368
Jay Patel <pateljay24004@gmail.com>
2021-08-11 21:52:08 +0530

    tab and space fixes

--- cogs/afk.py
+++ cogs/afk.py
@@ -85,10 +85,10 @@ class Afk(commands.Cog, description="An Afk commands"):
 		data['message'] = "⠀"
 		await self.bot.afk.upsert(data)
 		try:
-	    	self.bot.afk_user.pop(message.author.id)
+			self.bot.afk_user.pop(message.author.id)
 		except KeyError:
-		    pass#
-	    self.bot.afk_user[ctx.author.id] = data
+			pass#
+		self.bot.afk_user[user.id] = data
 
 
 
