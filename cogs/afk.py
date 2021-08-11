0f0e070162db9b3c1638b99b78130e20e757c436
Jay Patel <pateljay24004@gmail.com>
2021-08-11 20:39:06 +0530

    Bug fixes

--- cogs/afk.py
+++ cogs/afk.py
@@ -2,6 +2,9 @@ import discord
 import datetime
 from copy import deepcopy
 from discord.ext import commands
+import re
+
+mention_regex = re.compile("(@)(!|&)(\d\d\d)")
 
 class Afk(commands.Cog, description="An Afk commands"):
 	def __init__ (self, bot):
@@ -51,13 +54,16 @@ class Afk(commands.Cog, description="An Afk commands"):
 				user = await message.guild.fetch_member(value['_id'])
 				if user in message.mentions:
 					time = round(value['time'].timestamp())
-					await message.reply(f"User is afk {value['message']} -<t:{time}:R> <t:{time}:f>", mention_author=False)
+					await message.reply(f"{user.display_name} is afk {value['message']} -<t:{time}:R> <t:{time}:f>", mention_author=False)
 		
 	@commands.command(name="afk", description="set your stats afk with this command")
-	@commands.check_any(commands.has_any_role(785842380565774368, 803635405638991902, 799037944735727636, 785845265118265376, 787259553225637889, 843775369470672916, 818129661325869058))
+	@commands.check_any(commands.has_any_role(785842380565774368, 803635405638991902, 799037944735727636, 785845265118265376, 787259553225637889, 843775369470672916, 818129661325869058, 806804472700600400, 835866393458901033, 786477872029892639, 830013421239140403, 836551065733431348, 810129351692648479))
 	@commands.cooldown(1, 300, commands.BucketType.user)
 	async def afk(self, ctx, message=None):
 		message = message if message else "⠀"
+		match = re.findall(mention_regex, message)
+		if match:
+			return await ctx.reply("You can't have the any Role/User Mentions in your afk message")
 		data = {'_id': ctx.author.id,
 				'message': message,
 				'last_name': ctx.author.display_name,
@@ -66,7 +72,7 @@ class Afk(commands.Cog, description="An Afk commands"):
 			await ctx.author.edit(nick=f"[AFK] {ctx.author.display_name}")
 		except:
 			pass
-		await ctx.send("I have set your stats as afk")
+		await ctx.send("I have set your stats as afk")	
 		await self.bot.afk.upsert(data)
 		self.bot.afk_user[ctx.author.id] = data
 
