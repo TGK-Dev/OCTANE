5c947c67c5c6d1068c5e811a93713547a481428d
Jay Patel <pateljay24004@gmail.com>
2021-08-11 21:28:24 +0530

    typo fix

--- cogs/afk.py
+++ cogs/afk.py
@@ -27,7 +27,7 @@ class Afk(commands.Cog, description="An Afk commands"):
 			except KeyError:
 			    pass#🔓
 
-			await message.reply("your AFK stats has been Removed")
+			await message.reply("your AFK status has been Removed")
 			data = await self.bot.afk.find(message.author.id)
 			try:
 				await message.author.edit(nick=f"{data['last_name']}")
@@ -56,7 +56,7 @@ class Afk(commands.Cog, description="An Afk commands"):
 					time = round(value['time'].timestamp())
 					await message.reply(f"{user.display_name} is afk {value['message']} -<t:{time}:R> <t:{time}:f>", mention_author=False)
 		
-	@commands.command(name="afk", description="set your stats afk with this command")
+	@commands.command(name="afk", description="set your status afk with this command")
 	@commands.check_any(commands.has_any_role(785842380565774368, 803635405638991902, 799037944735727636, 785845265118265376, 787259553225637889, 843775369470672916, 818129661325869058, 806804472700600400, 835866393458901033, 786477872029892639, 830013421239140403, 836551065733431348, 810129351692648479))
 	@commands.cooldown(1, 300, commands.BucketType.user)
 	async def afk(self, ctx, *,message=None):
@@ -72,7 +72,7 @@ class Afk(commands.Cog, description="An Afk commands"):
 			await ctx.author.edit(nick=f"[AFK] {ctx.author.display_name}")
 		except:
 			pass
-		await ctx.send("I have set your stats as afk")	
+		await ctx.send("I have set your status as afk")	
 		await self.bot.afk.upsert(data)
 		self.bot.afk_user[ctx.author.id] = data
 
