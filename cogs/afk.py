546d621393d1292b91f73bfc28c36388954ce6d0
Jay Patel <pateljay24004@gmail.com>
2021-08-18 21:51:05 +0530

    auto afk bug fix maybe

--- cogs/afk.py
+++ cogs/afk.py
@@ -4,7 +4,8 @@ from copy import deepcopy
 from discord.ext import commands
 import re
 
-mention_regex = re.compile("(@)(!|&)(\d\d\d)")
+pc_mention_regex = re.compile("(@)(!|&)(\d\d\d)")
+mo_mention_regex = re.compile("<(@)")
 
 class Afk(commands.Cog, description="An Afk commands"):
 	def __init__ (self, bot):
@@ -27,12 +28,17 @@ class Afk(commands.Cog, description="An Afk commands"):
 			except KeyError:
 			    pass#🔓
 
-			await message.reply("your AFK status has been Removed")
+			await message.reply("your AFK status has been Removed", mention_author=False, delete_after=30)
 			data = await self.bot.afk.find(message.author.id)
 			try:
 				await message.author.edit(nick=f"{data['last_name']}")
 			except:
-				return await self.bot.afk.delete(message.author.id)
+				await self.bot.afk.delete(message.author.id)
+
+			try:
+			    self.bot.afk_user.pop(message.author.id)
+			except KeyError:
+			    return
 		
 
 		if type(message.reference) == None:
@@ -44,7 +50,7 @@ class Afk(commands.Cog, description="An Afk commands"):
 					user = await message.guild.fetch_member(value['_id'])
 					if msg.author.id == user.id:
 						time = round(value['time'].timestamp())
-						return await message.reply(f"{user.display_name} is afk {value['message']} -<t:{time}:R> <t:{time}:f>", mention_author=False)
+						return await message.reply(f"{user.display_name} is afk {value['message']} -<t:{time}:R> <t:{time}:f>", mention_author=False, delete_after=30)
 			except:
 				pass
 		if len(message.mentions) == 0:
@@ -54,14 +60,17 @@ class Afk(commands.Cog, description="An Afk commands"):
 				user = await message.guild.fetch_member(value['_id'])
 				if user in message.mentions:
 					time = round(value['time'].timestamp())
-					await message.reply(f"{user.display_name} is afk {value['message']} -<t:{time}:R> <t:{time}:f>", mention_author=False)
+					await message.reply(f"{user.display_name} is afk {value['message']} -<t:{time}:R> <t:{time}:f>", mention_author=False, delete_after=30)
 		
 	@commands.command(name="afk", description="set your status afk with this command")
 	@commands.check_any(commands.has_any_role(785842380565774368, 803635405638991902, 799037944735727636, 785845265118265376, 787259553225637889, 843775369470672916, 818129661325869058, 806804472700600400, 835866393458901033, 786477872029892639, 830013421239140403, 836551065733431348, 810129351692648479, 866739202069757963))
 	@commands.cooldown(1, 300, commands.BucketType.user)
 	async def afk(self, ctx, *,message=None):
 		message = message if message else "⠀"
-		match = re.findall(mention_regex, message)
+		match = re.findall(pc_mention_regex, message)
+		if match:
+			return await ctx.reply("You can't have the any Role/User Mentions in your afk message")
+		match = re.findall(mo_mention_regex, message)
 		if match:
 			return await ctx.reply("You can't have the any Role/User Mentions in your afk message")
 		data = {'_id': ctx.author.id,
@@ -85,10 +94,10 @@ class Afk(commands.Cog, description="An Afk commands"):
 		data['message'] = "⠀"
 		await self.bot.afk.upsert(data)
 		try:
-			self.bot.afk_user.pop(message.author.id)
+			self.bot.afk_user.pop(user.id)
 		except KeyError:
 			pass#
-		self.bot.afk_user[user.id] = dataa
+		self.bot.afk_user[user.id] = data
 		await ctx.send("User Afk message has been reset")
 
 
