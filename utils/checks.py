e6344d0fbb99e7b9b9eb4dfa58648716ce23f276
Jay Patel <pateljay24004@gmail.com>
2022-01-16 22:53:19 +0530

    new checks

---
+++ utils/checks.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+class checks():
+    
+    def is_me():
+        def predicate(ctx):
+            return ctx.author.id in [488614633670967307, 301657045248114690]
+        return commands.check(predicate)
+
+    def can_use():
+        async def predicate(ctx):
+            if ctx.author.id in [488614633670967307, 301657045248114690]: return True
+            command = await ctx.bot.active_cmd.find_by_custom({'command_name': ctx.command.name})
+            if not command: return await ctx.send("Error Happend Report to Jay")
+            user_roles = [role.id for role in ctx.author.roles]
+            if (set(user_roles) & set(command['allowed_roles'])):
+                return True
+            else:
+                return False
+        return commands.check(predicate)
