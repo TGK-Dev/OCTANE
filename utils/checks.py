8b00ac22c2e84806a067a84ecbd5a02877ac814b
Jay Patel <pateljay24004@gmail.com>
2022-01-22 17:57:35 +0530

    imporved check

--- utils/checks.py
+++ utils/checks.py
@@ -9,14 +9,21 @@ class checks():
 
     def can_use():
         async def predicate(ctx):
-            if ctx.author.id in [488614633670967307, 301657045248114690]: return True
             command = await ctx.bot.active_cmd.find_by_custom({'command_name': ctx.command.name})
-            if not command: return await ctx.send("Error Happend Report to Jay")
-            if command['disable'] == True: return await ctx.send("Command is disabled by Jay/Utki")
+            if type(command) != dict: return await ctx.send("Error Happend Report to Jay")
+
+            if command['disable'] == True:
+                await ctx.send("Command is disabled by Jay/Utki")
+                return False
+
+            if ctx.author.id in [488614633670967307, 301657045248114690]: return True
+
             user_roles = [role.id for role in ctx.author.roles]
             if ctx.author.id in command['allowed_users']: return True
+
             if (set(user_roles) & set(command['allowed_roles'])):
                 return True
             else:
                 return False
+                
         return commands.check(predicate)
