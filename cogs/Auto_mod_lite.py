0ddca0a82f8ebb6cea456fce0bb690c78e994622
Jay Patel <pateljay24004@gmail.com>
2022-01-23 17:05:35 +0530

    updated auto mod

--- cogs/Auto_mod_lite.py
+++ cogs/Auto_mod_lite.py
@@ -1,3 +1,4 @@
+from time import time
 import discord
 from discord.ext import commands
 import re
@@ -16,12 +17,33 @@ def is_link_bad(link: str):
     r = requests.post(url, data=json.dumps(payload)).json()
     return r
 
+class BanButton(discord.ui.View):
+    def __init__(self, bot):
+        self.bot = bot
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label='Ban User', style=discord.ButtonStyle.red, custom_id="BAN:BUTTON")
+    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+        msg = interaction.message.embeds[0].to_dict()
+        user = await self.bot.fetch_user(int(msg['footer']['text']))
+        if not user:
+            return await interaction.response.send_message("User not Found try with command")
+        await interaction.guild.ban(user, reason="phishing Links")
+        await interaction.response.send_message(f"User {user.name} is Banned")
+
+    async def interaction_check(self ,interaction):
+        if interaction.user.guild_permissions.ban_members:
+            return True
+        else:
+            await interaction.response.send_message("You don't Have permissions", ephemeral=True)
+
 class AutoMod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @commands.Cog.listener()
     async def on_ready(self):
+        self.bot.add_view(BanButton(self.bot))
         print(f'{self.__class__.__name__} Cog has been loaded\n-----')
     
     @commands.Cog.listener()
@@ -36,13 +58,16 @@ class AutoMod(commands.Cog):
             if r['badDomain'] == True:
                 embed = discord.Embed(description=f"Message Sent by {message.author.mention} Contains known phishing link",color=0xff0000)
                 embed.add_field(name="Message", value=f"||{message.content}||")
-                embed.add_field(name="Action Taken", value="Timeout User For 1 Day")
+                embed.add_field(name="Action Taken", value="Timeout User For 28 Day")
                 embed.set_footer(text=message.author.id)
                 time = datetime.datetime.utcnow()+ datetime.timedelta(days=27)
                 channel = self.bot.get_channel(803687264110247987)
-                await message.author.edit(timeout=time, reason="detection of phishing links")
-                await message.delete()
-                await channel.send(embed=embed, content="<@&787259553225637889> If link is phishing ban user Fast")
+                #await message.author.edit(timeout=time, reason="detection of phishing links")
+                try:
+                    await message.delete()
+                except:
+                    pass
+                await channel.send(embed=embed, content="<@&787259553225637889> If link is phishing ban user Fast", view=BanButton(self.bot))
                 try:
                     await message.author.send("phishing has been detected in your message and you have been put on timeout if you think it's mistake Dm any Admin")
                 except:
