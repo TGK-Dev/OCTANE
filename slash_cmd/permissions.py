1f28bac969d93973d915700195bbcb1cc28103dc
Jay Patel <pateljay24004@gmail.com>
2022-03-14 16:49:10 +0530

    Some Random Fixed for stable bot

--- slash_cmd/permissions.py
+++ slash_cmd/permissions.py
@@ -1,7 +1,7 @@
 from discord import app_commands
 import discord
 from typing import Union, Optional, List
-
+from discord.app_commands import Choice
 class Permissions(app_commands.Group):
     def __init__(self, bot, name: str = "permissons", parent=None):
         super().__init__(name=name, parent=parent)
@@ -85,5 +85,92 @@ class Permissions(app_commands.Group):
 
         return(choice[:24])
 
-    async def on_error(self, interaction: discord.Interaction, command, error):
-        pass
+    @app_commands.command(name="edit", description="Edit permissions of a command")
+    @app_commands.describe(command='command you want to edit')
+    @app_commands.describe(target="role or user")
+    @app_commands.describe(choice="Action you want to do")
+    @app_commands.choices(choice=[
+        Choice(name="Add", value=1),
+        Choice(name="Remove", value=2),
+        Choice(name="Disable", value=3)
+    ])
+    async def edit(
+        self, 
+        interaction: discord.Interaction, 
+        command: str, 
+        target: Union[discord.Member, discord.Role], 
+        choice: Choice[int]
+    ):
+        if not interaction.user.guild_permissions.administrator:
+            return await interaction.response.send_message("You don't have permission to use this command",ephemeral=True)
+            
+        command = self.bot.get_command(command)
+
+        if command is None: return await interaction.response.send_message("I can't find a command with that name!")
+
+        await interaction.response.defer(thinking=True)
+        cmd_data = await self.bot.active_cmd.find(command.name)
+
+        if not cmd_data:
+            cmd_data = {"_id": command.name, "allowed_roles": [], "allowed_users": [], "disable": False}
+            
+        if choice.value == 1:
+
+            if isinstance(target, discord.Role):
+                if target.id in cmd_data['allowed_roles']:
+                    return await interaction.followup.send("This role is already allowed")
+                else:
+                    cmd_data['allowed_roles'].append(target.id)
+                    await self.bot.active_cmd.update(cmd_data)
+                    await interaction.followup.send(f"Added {target.mention} to allowed roles", allowed_mentions=discord.AllowedMentions(roles=False))
+
+            if isinstance(target, discord.Member):
+                if target.id in cmd_data['allowed_users']:
+                    return await interaction.followup.send("This user is already allowed")
+                else:
+                    cmd_data['allowed_users'].append(target.id)
+                    await self.bot.active_cmd.update(cmd_data)
+                    await interaction.followup.send(f"Added {target.mention} to allowed users", allowed_mentions=discord.AllowedMentions(users=False))
+
+        if choice.value == 2:
+
+            if isinstance(target, discord.Role):
+                if target.id not in cmd_data['allowed_roles']:
+                    return await interaction.followup.send("This role is not allowed")
+                else:
+                    cmd_data['allowed_roles'].remove(target.id)
+                    await self.bot.active_cmd.update(cmd_data)
+                    await interaction.followup.send(f"Removed {target.mention} from allowed roles", allowed_mentions=discord.AllowedMentions(roles=False, users=False))
+            
+            if isinstance(target, discord.Member):
+                if target.id not in cmd_data['allowed_users']:
+                    return await interaction.followup.send("This user is not allowed")
+                else:
+                    cmd_data['allowed_users'].remove(target.id)
+                    await self.bot.active_cmd.update(cmd_data)
+                    await interaction.followup.send(f"Removed {target.mention} from allowed users", allowed_mentions=discord.AllowedMentions(roles=False, users=False))
+
+        if choice.value == 3:
+
+            if cmd_data['disable'] == True:
+                return await interaction.response.send_message("This command is already disabled")
+            else:
+                cmd_data['disable'] = True
+                await self.bot.active_cmd.update(cmd_data)
+                await interaction.followup.send("This command is disabled")
+        
+        self.bot.perm[command.name] = cmd_data
+        
+
+    @edit.autocomplete('command')
+    async def command_auto(self, interaction: discord.Interaction, current: str, namespace:app_commands.Namespace) -> List[app_commands.Choice[str]]:
+        current_command = [cmd.name for cmd in self.bot.commands]
+        choice = [
+            app_commands.Choice(name=cmd , value=cmd)
+            for cmd in current_command if current.lower() in cmd.lower()
+        ]
+
+        return(choice[:24])
+
+    # async def on_error(self, interaction: discord.Interaction, command, error):
+    #     return await interaction.response.send_message("An error occured", ephemeral=True)
