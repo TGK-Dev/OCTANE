51a526afecdc777fb85dbdcf2b55ce5dd5131d47
Jay Patel <pateljay24004@gmail.com>
2022-03-16 12:40:57 +0530

    Updated Anti Nuke

--- cogs/Anti_Nuke.py
+++ cogs/Anti_Nuke.py
@@ -11,6 +11,43 @@ class Anti_nuke(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'{self.__class__.__name__} Cog has been loaded\n-----')
+    
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+
+        if before.guild.id != 785839283847954433:
+            return
+        if before.roles == after.roles:
+            return
+        
+        new_role = set(after.roles) - set(before.roles)
+        if not new_role:
+            return
+
+        guild = after.guild
+        for role in new_role:
+            perms = [perm[0] for perm in role.permissions if perm[1] == True]
+            if (set(perms) & set(['kick_members', 'ban_members', 'administrator', 'manage_channels', 'manage_guild', 'manage_messages', 'manage_roles', 'manage_permissions'])):
+
+                logs = [log async for log in guild.audit_logs(limit=1, action=discord.AuditLogAction.member_role_update) if log.target.id == after.id]
+                if logs:
+
+                    if logs[0].user.id in [301657045248114690, 488614633670967307, 651711446081601545,457839031909351425,413651113485533194]:
+                        return
+                    else:
+                        await after.remove_roles(role)
+                        await after.edit(timed_out_until=discord.utils.utcnow() + datetime.timedelta(hours=10),reason="Possible Server Nuke attempt")
+                        for role in logs[0].user.roles:
+                            if role.permissions.administrator or role.permissions.manage_roles or role.permissions.kick_members or role.permissions.ban_members or role.permissions.manage_channels or role.permissions.manage_guild or role.permissions.manage_messages or role.permissions.manage_roles or role.permissions.manage_permissions:
+                                await logs[0].user.remove_roles(role, reason="Possible Server Nuke attempt")
+                        
+                        await logs[0].user.edit(timed_out_until=discord.utils.utcnow() + datetime.timedelta(hours=10), reason="Possible Server Nuke attempt")
+
+                        embed = discord.Embed(title="Possible Server Nuke attempt", description=f"{logs[0].user.mention} Has tryed to Give Moderator role to {after.mention}, Role is remove from {after.mention} and both user has put in Timeout", color=0xFF0000)
+                        embed.timestamp = discord.utils.utcnow()
+                        async with aiohttp.ClientSession() as session:
+                            webhook = Webhook.from_url(self.bot.nuke_webhook, session=session)
+                            await webhook.send(embed=embed, username=f"{self.bot.user.name} Anti-Nuke", avatar_url=self.bot.user.avatar.url, content="@everyone")
 
     @commands.Cog.listener()
     async def on_guild_role_update(self, before, after):
@@ -23,6 +60,7 @@ class Anti_nuke(commands.Cog):
                     guild = before.guild
                     role = discord.utils.get(guild.roles, id=before.id)
                     logs = await guild.audit_logs(limit=10, action=discord.AuditLogAction.role_update).flatten()
+                    logs = [log async for log in guild.audit_logs(limit=10, action=discord.AuditLogAction.role_update)]
                     for log in logs:
                         if log.target.id == before.id and log.user.id != self.bot.user.id:
                             log = log
