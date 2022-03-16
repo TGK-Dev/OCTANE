bc66bbda353ba00fbea2b1c3b60db215e214da33
Jay Patel <pateljay24004@gmail.com>
2022-03-16 17:31:51 +0530

    Removed Some command and Fix some commands

--- slash_cmd/Ticket.py
+++ slash_cmd/Ticket.py
@@ -233,11 +233,15 @@ class Ticket_Commands(app_commands.Group):
             overwrite.attach_files = False
 
             for i in interaction.channel.overwrites:
-                if type(i) == discord.Member and i.id == ticket_owner.id:
-                    pass
-                else:
-                    await interaction.channel.set_permissions(i, overwrite=overwrite)
+                await interaction.channel.set_permissions(i, overwrite=overwrite)
             
+            owner_owrite = discord.PermissionOverwrite()
+            owner_owrite.view_channel = True
+            owner_owrite.send_messages = True
+            owner_owrite.read_messages = True
+            owner_owrite.attach_files = True
+
+            await interaction.channel.set_permissions(ticket_owner, overwrite=owner_owrite)
 
             await interaction.followup.send("Ticket is now Admin Only")
 
@@ -292,65 +296,47 @@ class Ticket_Commands(app_commands.Group):
             log_embed.add_field(name="Action", value=f"Deleted {interaction.channel.name}")
             await log_channel.send(embed=log_embed)
 
+    # @app_commands.command(name="config", description="Configure the ticket system")
+    # @app_commands.describe(category="category of ticket")
+    # @app_commands.describe(support_role="support role of system")
+    # @app_commands.describe(pm_role="partner manager role of system")
+    # @app_commands.describe(transcript="transcript channel of system")
+    # @app_commands.describe(ticket_log="ticket log channel of system")
+    # async def config(self, interaction: Interaction, category: discord.CategoryChannel=None, support_role: discord.Role=None, pm_role: discord.Role=None, transcript: discord.TextChannel=None, ticket_log: discord.TextChannel=None):        
+    #     can_run = await checks.slash_check(self.bot, interaction, app_commands.command.__name__)
+    #     if can_run != True:
+    #         return await interaction.followup.send("You do not have permission to run this command")
 
-    @app_commands.command(name="stats", description="stats of tickets")
-    async def stats(self, interaction: Interaction):
-        data = await self.bot.ticket.find(interaction.channel.id)
-        if data is None and interaction.channel.category.id != self.bot.config_data[interaction.guild.id]["ticket_category"]:
-            return await interaction.response.send_message("This is not a ticket channel", ephemeral=True)
-        
-        embed = discord.Embed(title="Ticket Stats", color=0x2f3136)
-        embed.add_field(name="Ticket Owner", value=f"<@{data['ticket_owner']}>")
-        embed.add_field(name="Ticket Status", value=f"{data['status']}")
-        embed.add_field(name="Ticket Created", value=f"{data['created_at']}")
-        embed.add_field(name="Ticket Type", value=f"{data['type']}")
-        embed.add_field(name="Added Roles", value=f"{len(data['added_roles'])}")
-        embed.add_field(name="Added Users", value=f"{len(data['added_users'])}")
-        embed.color = 0x00FF00
-        
-        await interaction.response.send_message(embed=embed)
+    #     data = await self.bot.config.find(interaction.guild.id)
+    #     if data is None:
+    #         data = {'_id': interaction.guild.id, 'ticket_category': None, 'support_role': None}
 
-    @app_commands.command(name="config", description="Configure the ticket system")
-    @app_commands.describe(category="category of ticket")
-    @app_commands.describe(support_role="support role of system")
-    @app_commands.describe(pm_role="partner manager role of system")
-    @app_commands.describe(transcript="transcript channel of system")
-    @app_commands.describe(ticket_log="ticket log channel of system")
-    async def config(self, interaction: Interaction, category: discord.CategoryChannel=None, support_role: discord.Role=None, pm_role: discord.Role=None, transcript: discord.TextChannel=None, ticket_log: discord.TextChannel=None):        
-        can_run = await checks.slash_check(self.bot, interaction, app_commands.command.__name__)
-        if can_run != True:
-            return await interaction.followup.send("You do not have permission to run this command")
+    #     if category:
+    #         data['ticket_category'] = category.id
 
-        data = await self.bot.config.find(interaction.guild.id)
-        if data is None:
-            data = {'_id': interaction.guild.id, 'ticket_category': None, 'support_role': None}
+    #     if support_role:
+    #         data['support_role'] = support_role.id
 
-        if category:
-            data['ticket_category'] = category.id
+    #     if pm_role:
+    #         data['pm_role'] = pm_role.id
 
-        if support_role:
-            data['support_role'] = support_role.id
+    #     if transcript:
+    #         data['transcript_log_channel'] = transcript.id
+    #     if ticket_log:
+    #         data['ticket_log_channel'] = ticket_log.id
 
-        if pm_role:
-            data['pm_role'] = pm_role.id
-
-        if transcript:
-            data['transcript_log_channel'] = transcript.id
-        if ticket_log:
-            data['ticket_log_channel'] = ticket_log.id
-
-        if support_role or category or pm_role or transcript or ticket_log:
-            await self.bot.config.upsert(data)
-            await interaction.response.send_message("Configuration Updated")
-        else:
-            embed = discord.Embed(title="Ticket Config", color=0x2f3136)
-            embed.add_field(name="Ticket Category", value=f"<#{data['ticket_category']}>")
-            embed.add_field(name="Support Role", value=f"<@&{data['support_role']}>")
-            embed.add_field(name="Partner Manager Role", value=f"<@&{data['pm_role']}>")
-            embed.add_field(name="Transcript Log Channel", value=f"<#{data['transcript_log_channel']}>")
-            embed.add_field(name="Ticket Log Channel", value=f"<#{data['ticket_log_channel']}>")
-            embed.set_footer(text="Made by Jay and utki007")
-            await interaction.response.send_message(embed=embed)
+    #     if support_role or category or pm_role or transcript or ticket_log:
+    #         await self.bot.config.upsert(data)
+    #         await interaction.response.send_message("Configuration Updated")
+    #     else:
+    #         embed = discord.Embed(title="Ticket Config", color=0x2f3136)
+    #         embed.add_field(name="Ticket Category", value=f"<#{data['ticket_category']}>")
+    #         embed.add_field(name="Support Role", value=f"<@&{data['support_role']}>")
+    #         embed.add_field(name="Partner Manager Role", value=f"<@&{data['pm_role']}>")
+    #         embed.add_field(name="Transcript Log Channel", value=f"<#{data['transcript_log_channel']}>")
+    #         embed.add_field(name="Ticket Log Channel", value=f"<#{data['ticket_log_channel']}>")
+    #         embed.set_footer(text="Made by Jay and utki007")
+    #         await interaction.response.send_message(embed=embed)
     
     @app_commands.command(name='save', description="save transcript of ticket")
     @app_commands.describe(topic="topic of ticket")
