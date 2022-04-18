bd317efad9c89d0991415bf0cf307c9f3a41bdf8
Jay Patel <pateljay24004@gmail.com>
2022-04-18 23:01:03 +0530

    Upated Guild config template

--- utils/functions.py
+++ utils/functions.py
@@ -1,3 +1,5 @@
 
 def make_db_temp(id):
-        return {'_id': id, 'blacklist': [], 'welcome': None, 'mod_log': None, 'case': 1, 'starboard': {'channel': None, 'threshold': 5, 'self_star': False, 'toggle': False}, 'suggestion': None}
+        return {'_id': id, 'blacklist': [], 'welcome': None, 'mod_log': None, 'case': 1, 'starboard': {'channel': None, 'threshold': 5, 'self_star': False, 'toggle': False}, 'suggestion': None,
+                "Tickets": {'category': None, 'log_channel': None, 'partner_ship': {'support_roles': [], 'ping_roles': None}, 'support': {'support_roles': [], 'ping_roles': None}}
+        }
