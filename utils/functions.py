0bf9b5c23cba35575ca8b4db5678e1d3d3103a47
Jay Patel <pateljay24004@gmail.com>
2022-04-19 21:38:27 +0530

    Added: More Founctions

--- utils/functions.py
+++ utils/functions.py
@@ -3,3 +3,9 @@ def make_db_temp(id):
         return {'_id': id, 'blacklist': [], 'welcome': None, 'mod_log': None, 'case': 1, 'starboard': {'channel': None, 'threshold': 5, 'self_star': False, 'toggle': False}, 'suggestion': None,
                 "Tickets": {'category': None, 'log_channel': None, 'partner_ship': {'support_roles': [], 'ping_roles': None}, 'support': {'support_roles': [], 'ping_roles': None}}
         }
+
+def clean_code(content):
+    if content.startswith("```") and content.endswith("```"):
+        return "\n".join(content.split("\n")[1:])[:-3]
+    else:
+        return content
