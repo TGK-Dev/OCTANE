d818f51c5cb9fe60a5d4816a07460a831256675d
Jay Patel <pateljay24004@gmail.com>
2022-06-19 17:50:33 +0530

    New Ar done

--- cogs/auto_response.py
+++ cogs/auto_response.py
@@ -35,9 +35,10 @@ class Ar(commands.Cog, name="Auto Responce", description="Easy way to add Auto R
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot or self.heist_ar_active == False or message.guild.id != 785839283847954433 and message.channel.id == 855717169584537660:
+        if message.author.bot or self.heist_ar_active == False or message.guild.id != 785839283847954433:
             return
         
+        if message.channel.id not in [785847439579676672, 799364834927968336,799378297855279125]: return
         # if message.author.guild_permissions.manage_messages:
         #     return
         
@@ -45,6 +46,8 @@ class Ar(commands.Cog, name="Auto Responce", description="Easy way to add Auto R
             embed = discord.Embed(description=f"> **{self.heist_ar['ammount']} Heist** **begins** <t:{round(self.heist_ar['time'].timestamp())}:R>")
             if self.heist_ar['role'] != None:
                 embed.description += "\n> **Required Role:** <@&{}>".format(self.heist_ar['role'])
+            if self.heist_ar['bypass_role'] != None:
+                embed.description += "\n> **Bypass Role:** <@&{}>".format(self.heist_ar['bypass_role'])
             embed.color = discord.Color.random()
             await message.reply(f"Heist will take place in <#{self.heist_ar['channel']}>!!",embed=embed, delete_after=10)
 
@@ -52,7 +55,7 @@ class Ar(commands.Cog, name="Auto Responce", description="Easy way to add Auto R
     @app_commands.guild_only()
     @app_commands.guilds(785839283847954433)
     @app_commands.rename(remove_old="overwrite")
-    async def set_heist_ar(self, interaction: Interaction, time: str, channel: discord.TextChannel, amount: str, role: discord.Role=None, remove_old: bool=False):
+    async def set_heist_ar(self, interaction: Interaction, time: str, channel: discord.TextChannel, amount: str, role: discord.Role=None, remove_old: bool=False, bypass_role:discord.Role=None):
         times = await TimeConverter().convert(interaction, time)
         if time == 0:
             await interaction.response.send_message("Time is 0, no auto responce will be set", ephemeral=True)
@@ -63,7 +66,7 @@ class Ar(commands.Cog, name="Auto Responce", description="Easy way to add Auto R
             return
         
         time = datetime.datetime.now() + datetime.timedelta(seconds=times)
-        self.heist_ar = {'_id': interaction.user.id, 'time': time, 'channel': channel.id, 'ammount': amount, 'role': role.id if role else None, "expir": times}
+        self.heist_ar = {'_id': interaction.user.id, 'time': time, 'channel': channel.id, 'ammount': amount, 'role': role.id if role else None, "expir": times, "bypass_role": bypass_role.id if bypass_role else None}
         await interaction.response.send_message("Auto Responce has been set", ephemeral=True)
         self.heist_ar_active = True
         print(self.heist_ar)
