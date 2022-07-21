ddba67d4e7a498164fa30afbb4916b50c7fe30b6
Jay Patel <pateljay24004@gmail.com>
2022-07-21 12:18:20 +0530

    Added Logger

--- main.py
+++ main.py
@@ -11,7 +11,19 @@ import asyncio
 import logging
 import datetime
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 main_guilds = [785839283847954433, 811037093715116072]
 
