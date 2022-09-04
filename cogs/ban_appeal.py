8367b9edbb4bc43b543b60f4238d6f0546207d88
Jay Patel <pateljay24004@gmail.com>
2022-09-04 16:45:13 +0530

    added cmd to check ban reason in apeal server

--- cogs/ban_appeal.py
+++ cogs/ban_appeal.py
@@ -12,10 +12,23 @@ class BanAppeal(commands.Cog, name="Ban Appeal", description="Easy way to add Ba
     async def on_ready(self):
         print(f"{self.__class__.__name__} Cog has been loaded\n-----")
 
+    @app_commands.command(name='check', description='check reason of ban from main server')
+    @app_commands.checks.has_any_role(988764089713066005)
+    async def check(self, interaction: discord.Interaction, user: discord.Member):
+        user = await interaction.client.fetch_user(user.id)
+        main_guild = interaction.client.get_guild(785839283847954433)
+        try:
+            ban = await main_guild.fetch_ban(user)
+            embed = discord.Embed(description=f"Reason: {ban.reason}", color=0x00ff00)
+            await interaction.response.send_message(embed=embed)
+        except:
+            embed = discord.Embed(description="ban not found", color=0xff0000)
+            await interaction.response.send_message(embed=embed)
+
     @app_commands.command(name="approve", description="Approve a ban appeal")
     @app_commands.guild_only()
     @app_commands.guilds(988761284956799038)
-    @app_commands.default_permissions(administrator=True)
+    @app_commands.checks.has_any_role(988764089713066005)
     async def approve(self, interaction: Interaction, user: discord.Member):
         main_guild = await self.bot.fetch_guild(785839283847954433)
         try:
@@ -23,15 +36,14 @@ class BanAppeal(commands.Cog, name="Ban Appeal", description="Easy way to add Ba
             if ban:
                 await main_guild.unban(user, reason="Ban Appeal Approved by {}".format(interaction.user.name))
                 await interaction.response.send_message("Ban Appeal Approved", ephemeral=True)
-                await interaction.channel.send(f"{user.mention} Your ban has been lifted by {interaction.user.mention}")
+                await interaction.channel.send(f"{user.mention} Your ban has been lifted by {interaction.user.mention}, you can now rejoin the server at https://discord.gg/uJeHDqpCVw")
         except discord.NotFound:
             await interaction.response.send_message("User is not banned")
             return        
 
     @app_commands.command(name="deny", description="Deny a ban appeal")
     @app_commands.guild_only()
-    @app_commands.guilds(988761284956799038)
-    @app_commands.default_permissions(administrator=True)
+    @app_commands.checks.has_any_role(988764089713066005)
     async def deny(self, interaction: Interaction, user: discord.Member, reason: str=None):
         main_guild = await self.bot.fetch_guild(785839283847954433)
         try:
