dc58ccd27757d0f9bc6b4747c5597bcaa9b88bde
Jay Patel <pateljay24004@gmail.com>
2022-10-13 18:27:30 +0530

    finished /work shift and /crime reminders

--- cogs/dankremider.py
+++ cogs/dankremider.py
@@ -14,6 +14,7 @@ class DankReminder(commands.GroupCog, name="dankreminder"):
         self.bot.dank_reminders = Document(self.bot.db, "dank_reminders")
         self.dank_reminder_task = self.dank_reminder.start()
         self.command_info = {"work_shift": "</work shift:1011560371267579942>", "crime":"</crime:1011560371078832202>"}
+        self.message_in_progress = []
     
     def cog_unload(self):
         self.dank_reminder_task.cancel()
@@ -98,26 +99,12 @@ class DankReminder(commands.GroupCog, name="dankreminder"):
             data['reminders'][_type]['last_reminder'] = datetime.datetime.now()
             data['reminders'][_type]['reminded'] = True
             await self.update_data(self, user, data)
-    
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if not message.author.bot: return
-        if message.guild is None: return
-        if message.guild.id != 785839283847954433: return
-        if message.author.id != 270904126974590976: return
-        if message.interaction is None: return
-
-        if message.interaction.name == "work shift":
-            self.bot.dispatch("dank_wrok_shift", message)
-        if message.interaction.name == "crime":
-            self.bot.dispatch("dank_crime", message)
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
+        if after.id in self.message_in_progress: return
         if not after.author.bot: return
         if after.guild is None: return
-        if after.guild.id != 785839283847954433: return
-        if after.channel.id != 999575712736497695: return
 
         if after.interaction is None: return
 
@@ -125,13 +112,16 @@ class DankReminder(commands.GroupCog, name="dankreminder"):
             embed = after.embeds[0]
             if embed.title == "Terrible work!" or "Great work!":
                 self.bot.dispatch("dank_workShift_done", after, after.interaction)
+                self.message_in_progress.append(after.id)
         
         if after.interaction.name == "crime":
             if after.components[0].children[0].disabled == True and after.components[0].children[-1].disabled == True:
                 self.bot.dispatch("dank_crime_done", after, after.interaction)
+                self.message_in_progress.append(after.id)
 
     @commands.Cog.listener()
     async def on_dank_crime_done(self, message, interaction):
+        if message.id in self.message_in_progress: return
         user = interaction.user
         data = await self.get_cache(self, user)
         if "crime" not in data['reminders'].keys():
@@ -172,14 +162,17 @@ class DankReminder(commands.GroupCog, name="dankreminder"):
                 data['reminders']['crime']['reminded'] = False
                 await self.update_data(self, user, data)
                 await message.add_reaction("<:octane_yes:1019957051721535618>")
+
+        self.message_in_progress.remove(message.id)
     
     @commands.Cog.listener()
     async def on_dank_workShift_done(self, message, interaction):
+        print("work shift done")
         user = interaction.user
         data = await self.get_cache(self, user)
-        if not message.embeds[0].title == "Great work!" or "Terrible work!": return
         if not data["enabled"]: return
         if "work_shift" not in data["reminders"].keys():
+            print("work shift not in data adding")
             view = Confirm(user, 60)
             msg = await message.reply(f"Hey {user.mention}, you don't have a reminder set for `/work shift`, do you want to set one?", view=view)
             view.message = msg
@@ -212,54 +205,69 @@ class DankReminder(commands.GroupCog, name="dankreminder"):
                 await self.update_data(self, user, data)
                 await message.add_reaction("<:octane_no:1019957208466862120>")
                 await view.message.edit(view=None)
-            return
+
+                self.message_in_progress.remove(message.id)
+                return
         else:
+            print("work shift in data updating")
             if data['reminders']['work_shift']['enabled'] == False: return
             data['reminders']['work_shift']['last_used'] = datetime.datetime.now()
             data['reminders']['work_shift']['next_reminder'] = datetime.datetime.now() + datetime.timedelta(hours=1)
             data['reminders']['work_shift']['reminded'] = False
             await self.update_data(self, user, data)
             await message.add_reaction("<:octane_yes:1019957051721535618>")
-            return
+        
+        self.message_in_progress.remove(message.id)
 
     @app_commands.command(name="toggle", description="Toggle a reminder")
-    async def toggle(self, interaction: discord.Interaction, command: Literal['work'], enabled: bool):
-        data = await self.get_userdata(self, interaction.user)
-        if command not in data['reminders'].keys():
-            data['reminders'][command] = {
-                "enabled": True,
-                "reminded": False,
-                "next_reminder": None,
-                "channel": None,
-                "guild_id": interaction.guild.id
-            }
+    @app_commands.describe(command="Command to toggle a reminder", enabled="Enable or disable a reminder")
+    @app_commands.choices(command=[app_commands.Choice(name="All command", value="global"), app_commands.Choice(name="work shift", value="work_shift"), app_commands.Choice(name="crime", value="crime")])
+    async def toggle(self, interaction: discord.Interaction, command: app_commands.Choice[str], enabled: bool):
+        data = await self.get_cache(self, interaction.user)
+        if command == "global":
+            data["enabled"] = enabled
+            await self.update_data(self, interaction.user, data)
+            embed = discord.Embed(description=f"Alright, I have turned all reminders **{'<:Toggle_on:1029771260114243584>' if enabled else '<:Toggle_off:1029770614430498926>'}**", color=discord.Color.red() if not enabled else discord.Color.green())
+            await interaction.response.send_message(embed=embed)
+            return
         
-        if enabled is True:
-            data['reminders'][command]['enabled'] = True
-            await interaction.response.send_message("Reminder enabled!", ephemeral=True)
+        if command.value not in data["reminders"].keys():
+            await interaction.response.send_message(f"You don't have a reminder set for `{command.value.replace('_', ' ')}`, use that command to set one up!", ephemeral=True)
         else:
-            data['reminders'][command]['enabled'] = False
-            await interaction.response.send_message("Reminder disabled!", ephemeral=True)
-
-        await self.bot.dank_reminders.update(data)
-        self.bot.dank_reminders_cache[data["_id"]] = data
+            data["reminders"][command.value]["enabled"] = enabled
+            await self.update_data(self, interaction.user, data)
+            embed = discord.Embed(description=f"Alright, I have turned the `{command.value.replace('_', ' ')}` reminder **{'<:Toggle_on:1029771260114243584>' if enabled else '<:Toggle_off:1029770614430498926>'}**", color=discord.Color.red() if not enabled else discord.Color.green())
+            await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="view", description="View your reminders")
     async def view(self, interaction: discord.Interaction):
         data = await self.get_userdata(self, interaction.user)
         embed = discord.Embed(title="Dank Reminders", color=0x2F3136)
         for reminder in data['reminders'].keys():
-            value = f"<:nat_reply:1011501024625827911> Enabled: {'<:Toggle_on:1029771260114243584>' if data['reminders'][reminder]['enabled'] else '<:Toggle_off:1029770614430498926>'}\n"
-            value += f"<:nat_reply:1011501024625827911> Reminded: {'<:octane_yes:1019957051721535618>' if data['reminders'][reminder]['reminded'] else '<:octane_no:1019957208466862120>'}\n"
+            value = f"<:nat_reply:1011501024625827911> **Enabled:** {'<:Toggle_on:1029771260114243584>' if data['reminders'][reminder]['enabled'] else '<:Toggle_off:1029770614430498926>'}\n"
+            value += f"<:nat_reply:1011501024625827911> **Reminded:** {'<:octane_yes:1019957051721535618>' if data['reminders'][reminder]['reminded'] else '<:octane_no:1019957208466862120>'}\n"
             if data['reminders'][reminder]['next_reminder'] is not None:
-                value += f"<:nat_reply:1011501024625827911> Next reminder: <t:{round(data['reminders'][reminder]['next_reminder'].timestamp())}:R>\n"
+                value += f"<:nat_reply:1011501024625827911> **Next reminder:** <t:{round(data['reminders'][reminder]['next_reminder'].timestamp())}:R>\n"
             else:
-                value += "<:nat_reply:1011501024625827911> <:nat_reply:1011501024625827911> Next reminder: None\n"
+                value += "<:nat_reply:1011501024625827911> <:nat_reply:1011501024625827911> **Next reminder:** None\n"
             if data['reminders'][reminder]['last_reminder'] is not None:
-                value += f"<:nat_reply_cont:1011501118163013634> Last reminder: <t:{round(data['reminders'][reminder]['last_reminder'].timestamp())}:R>\n"
+                value += f"<:nat_reply_cont:1011501118163013634> **Last reminder:** <t:{round(data['reminders'][reminder]['last_reminder'].timestamp())}:R>\n"
             else:
-                value += "<:nat_reply_cont:1011501118163013634> Last reminder: None\n"
-            embed.add_field(name=reminder.capitalize(), value=value, inline=False)
+                value += "<:nat_reply_cont:1011501118163013634> **Last reminder:** None\n"
+            
+            name = reminder.replace('_', ' ')
+            embed.add_field(name=name.capitalize(), value=value, inline=False)
         await interaction.response.send_message(embed=embed, ephemeral=False)
+    
+    @app_commands.command(name="bug", description="Report a bug in the reminders system")
+    @app_commands.describe(bug="The bug you want to report")
+    async def bug(self, interaction: discord.Interaction, bug: str):
+        await interaction.response.send_message("Bug reported!", ephemeral=True)
+        embed = discord.Embed(title="Bug Report", description=bug, color=0x2F3136)
+        embed.set_footer(text=f"Reported by {interaction.user} ({interaction.user.id})", icon_url=interaction.user.avatar.url)
+        guild = self.bot.get_guild(785839283847954433)
+        channel = guild.get_channel(1030096468121493524)
+        await channel.send(embed=embed)
+
 async def setup(bot):
-    await bot.add_cog(DankReminder(bot), guilds=[discord.Object(785839283847954433)])
+    await bot.add_cog(DankReminder(bot), guilds=[discord.Object(785839283847954433), discord.Object(811037093715116072)])
