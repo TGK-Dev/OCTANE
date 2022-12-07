5659c56390a568da9b40f13a9975b3050b5d1e76
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-12-07 14:24:10 +0530

    Update buttons.py

--- ui/buttons.py
+++ ui/buttons.py
@@ -126,7 +126,7 @@ class Payout_Buttton(discord.ui.View):
 
             guild = interaction.guild
             gk = self.bot.get_guild(785839283847954433)
-            paidemoji = await gk.fetch_emoji(1035061256073248849)
+            paidemoji = await guild.fetch_emoji(1035061256073248849)
             channel = guild.get_channel(int(data["channel"]))
             try:
                 message = await channel.fetch_message(data["_id"])
